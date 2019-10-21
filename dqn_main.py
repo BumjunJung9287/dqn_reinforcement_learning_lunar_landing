f1bbc65410a1bf76e762a8077178ec1d4cb27d78
Bumjun Jung <47442084+BumjunJung9287@users.noreply.github.com>
2019-10-21 14:52:41 +0900

    Add files via upload

---
+++ dqn_main.py
@@ -0,0 +1,39 @@
+import gym
+from simple_dqn_torch import Agent
+#from utils import plotLearning
+import numpy as np
+#from gym import wrappers
+
+if __name__ == '__main__':
+    env = gym.make('LunarLander-v2')
+    brain = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4,
+                    input_dims=[8], lr=0.003)
+    scores = []
+    eps_history = []
+    n_games = 500
+    score = 0
+
+    for i in range(n_games):
+        if i % 10 == 0 and i>0:
+            avg_score = np.mean(scores[max(0,i-10):(i+1)])
+            print("episode", i, "score", score,
+                    "average score %.3f" % avg_score,
+                    "epsilon %.3f" % brain.epsilon)
+        else:
+            print("episode ", i, "score", score)
+        score = 0
+        eps_history.append(brain.epsilon)
+        observation = env.reset()
+        done = False
+        while not done:
+            action = brain.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            score += reward
+            brain.store_transition(observation, action, reward, observation_, done)
+            observation = observation_
+            brain.learn()
+        scores.append(score)
+
+    x = [i+1 for i in range(n_games)]
+    #filename = "lunar-lander.png"
+    #plotLearning(x, scores, eps_history, filename)
